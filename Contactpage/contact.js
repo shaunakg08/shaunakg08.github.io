8ed73b807286135098f6f8480cd733c35758d97a
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-08-15 12:26:46 +0530

    updated html files

--- Contactpage/contact.js
+++ Contactpage/contact.js
@@ -26,3 +26,29 @@ var app = function() {
 
     init();
 }();
+
+function sendMessage() {
+    var form = document.getElementById("fcf-form-id")
+    var data = new FormData(form)
+    if (data.get("name") == undefined || data.get("name") == null || data.get("name").trim() == "") {
+        return alert("Please enter your name!")
+    }
+    if (data.get("email") == undefined || data.get("email") == null || data.get("email").trim() == "") {
+        return alert("Please enter your email address!")
+    }
+    if (data.get("message") == undefined || data.get("message") == null || data.get("message").trim() == "") {
+        return alert("Please enter your message!")
+    }
+    var sendButton = document.getElementById("fcf-button")
+    sendButton.setAttribute("disabled", "true")
+    sendButton.nodeValue = "Sending..."
+    __sendMessage(data, (errorMessage) => {
+        alert(errorMessage)
+        sendButton.removeAttribute("disabled")
+        sendButton.nodeValue = "Send Message"
+    }, () => {
+        alert("Message sent!")
+        sendButton.removeAttribute("disabled")
+        sendButton.nodeValue = "Send Message"
+    })
+}
