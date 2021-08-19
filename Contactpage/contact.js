cddd337b113237f1c5e4e9170dd6f51e6cef332b
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-08-19 12:38:06 +0530

    corrected form submission

--- Contactpage/contact.js
+++ Contactpage/contact.js
@@ -34,7 +34,15 @@ var app = function() {
 function sendMessage() {
     var form = document.getElementById("fcf-form-id")
     var data = new FormData(form)
-
+    if (data.get("name") == undefined || data.get("name") == null || data.get("name").trim() == "") {
+        return alert("Please enter your name!")
+    }
+    if (data.get("email") == undefined || data.get("email") == null || data.get("email").trim() == "") {
+        return alert("Please enter your email address!")
+    }
+    if (data.get("message") == undefined || data.get("message") == null || data.get("message").trim() == "") {
+        return alert("Please enter your message!")
+    }
     var sendButton = document.getElementById("fcf-button")
     sendButton.setAttribute("disabled", "true")
     sendButton.nodeValue = "Sending..."
